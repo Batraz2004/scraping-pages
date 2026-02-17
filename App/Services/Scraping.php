5409861cab5e97e74951bf549e87704295cf95c3
Batraz2004 <batraz.tolasovv@gmail.com>
2026-02-17 18:21:08 +0300

    refactoring scraping file

--- App/Services/Scraping.php
+++ App/Services/Scraping.php
@@ -30,46 +30,32 @@ class Scraping
         $doc->loadHTML($htmlString);
         $xpath = new DOMXPath($doc);
 
-        $query = "//body/*";
+        $query = "//body//text()[not(ancestor::script) and not(ancestor::style)]"; //не являющиеся потомками script и style
 
         $allElements = $xpath->query($query);
 
         $extractedWords = [];
 
-        // предложения
+        //весь текст тегов
+        $fullTextContent = "";
 
         foreach ($allElements as $el) {
-            try {
-                if (in_array($el->nodeName, ['script', 'style'])) {
-                    continue;
+            $fullTextContent .= $el->textContent;
+        }
+
+        $fullTextContent = preg_replace('/\s\s+/', ' ', $fullTextContent); //убрать лишние отступы
+        $fullTextContent = explode(" ", $fullTextContent);
+
+        foreach ($fullTextContent as $word) {
+            $word = trim($word); //удалить пробелы у слова
+            $textContentOfNodeArr = explode(" ", $word);
+            foreach ($textContentOfNodeArr as $val) {
+                if (
+                    strlen($val) > 0
+                    && preg_match("/^(?=.*[A-Za-z])[\dA-Za-z!@.,;:=_-]*$/", $val) //нужно что бы строка начиналась ли цифры или с англиской буквы а потом может содержать разные знаки разделители, стркоа обязательно должна иметь буквы
+                ) {
+                    $extractedWords[] = $val;
                 }
-
-                //текст или предложение
-                $textContentOfNode = $el->textContent;
-                $textContentOfNode = preg_replace("/(^[\r\n]*|[\r\n]+)[\s\t]*[\r\n]+/", "", $textContentOfNode); //убрать пустые строки в тексте
-                $textContentOfNode = preg_replace('/\s\s+/', ' ', $textContentOfNode); //убрать лишние отступы
-
-                //по словам
-                $textContentOfNodeArr = explode("\n\r", $textContentOfNode);
-
-                foreach ($textContentOfNodeArr as $word) {
-                    $word = trim($word); //удалить пробелы у слова
-                    $textContentOfNodeArr = explode(" ", $word);
-                    foreach ($textContentOfNodeArr as $val) {
-                        if (
-                            strlen($val) > 0
-                            && preg_match("/^(?=.*[A-Za-z])[\dA-Za-z!@.,;:=_-]*$/", $val) //нужно что бы строка начиналась ли цифры или с англиской буквы а потом может содержать разные знаки разделители, стркоа обязательно должна иметь буквы
-                        ) {
-                            // $word = preg_replace('/\s+/', "\n\r", $word);
-                            // $word = trim($word, " \n\t\r\v");
-                            $extractedWords[] = $val;
-                        }
-                    }
-                }
-            } catch (Throwable $ex) {
-                $errorScrapingLogsFilePath = 'App/Logs/Scraping.Log';
-                $message = "не удалось спарсить элемент {$el?->nodeName} типа {$el?->nodeType} со значением {$el?->nodeValue} на странице: {$url}, ошибка:{$ex?->getMessage()}. код:{$ex?->getCode()} в файле:{$ex?->getFile()} на строке: {$ex?->getLine()} \n";
-                file_put_contents($errorScrapingLogsFilePath, $message, FILE_APPEND);
             }
         }
 
