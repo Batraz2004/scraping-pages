92a91bb2ff70d647056a60293ecc7fa9323dcaa8
Batraz2004 <batraz.tolasovv@gmail.com>
2026-02-19 16:00:50 +0300

    scraping speed boosted with guzzle multiple requests (pooling)

--- App/Services/Scraping.php
+++ App/Services/Scraping.php
@@ -5,57 +5,75 @@ namespace App\Services;
 use DOMDocument;
 use DOMXPath;
 use Exception;
+use GuzzleHttp\Exception\RequestException;
+use GuzzleHttp\Pool;
+use GuzzleHttp\Psr7\Request;
+use GuzzleHttp\Psr7\Response;
 
 class Scraping
 {
     public function __construct(public \GuzzleHttp\Client $httpClient) {}
 
-    public function procces($url): array
+    public function procces($urls, &$pageFailUrlsByScraping): array
     {
-        if (!filter_var($url, FILTER_VALIDATE_URL)) {
-            echo "не корректный url";
-            throw new Exception("не корректный url", 422);
-        }
-
-        $httpClient = $this->httpClient;
-
-        $response = $httpClient->get($url);
-        $htmlString = (string) $response->getBody(); // содержит документ в виде строки
-
-        //навигация по тегам
-        libxml_use_internal_errors(true);
-        $doc = new DOMDocument();
-        $doc->preserveWhiteSpace = false;
-        $doc->loadHTML($htmlString);
-        $xpath = new DOMXPath($doc);
-
-        $query = "//body//text()[not(ancestor::script) and not(ancestor::style)]"; //не являющиеся потомками script и style
-
-        $allElements = $xpath->query($query);
+        $requests = function () use ($urls) {
+            foreach ($urls as $url) {
+                yield new Request('GET', $url);
+            }
+        };
 
         $extractedWords = [];
+        $errorScrapingLogsFilePath = 'App/Logs/Scraping.Log';
 
-        //весь текст тегов
-        $fullTextContent = "";
+        $pool = new Pool($this->httpClient, $requests(), [
+            'concurrency' => 10,
+            'fulfilled' => function (Response $response, $index) use ($urls, &$extractedWords) {
+                $htmlString = (string)$response->getBody();
 
-        foreach ($allElements as $el) {
-            $fullTextContent .= $el->textContent;
-        }
+                //навигация по тегам
+                libxml_use_internal_errors(true);
+                $doc = new DOMDocument();
+                $doc->preserveWhiteSpace = false;
+                $doc->loadHTML($htmlString);
+                $xpath = new DOMXPath($doc);
 
-        $fullTextContent = preg_replace('/\s+/', ' ', $fullTextContent); //убрать лишние отступы и нормализовать пробелы
-        $fullTextContent = explode(" ", $fullTextContent);
+                $query = "//body//text()[not(ancestor::script) and not(ancestor::style)]"; //не являющиеся потомками script и style
 
-        foreach ($fullTextContent as $word) {
-            if (
-                strlen($word) > 0
-                // && preg_match("/^(?=.*[A-Za-z])[\dA-Za-z\"'!@.,;:=_-]*$/", $word) //нужно что бы строка начиналась ли цифры или с англиской буквы а потом может содержать разные знаки разделители, стркоа обязательно должна иметь буквы
-                && preg_match("/^(?=.*[A-Za-z])[\dA-Za-z[:punct:]]*$/", $word) //нужно что бы строка начиналась ли цифры или с англиской буквы а потом может содержать разные спец символы(!"#$%&'()*+,-./:;<=>?@[\]^_\`{|}~), стркоа обязательно должна иметь буквы
-            ) {
-                $extractedWords[] = $word;
+                $allElements = $xpath->query($query);
+
+                //весь текст тегов
+                $fullTextContent = "";
+
+                foreach ($allElements as $el) {
+                    $fullTextContent .= $el->textContent;
+                }
+
+                $fullTextContent = preg_replace('/\s+/', ' ', $fullTextContent); //убрать лишние отступы и нормализовать пробелы
+                $fullTextContent = explode(" ", $fullTextContent);
+
+                foreach ($fullTextContent as $word) {
+                    if (
+                        strlen($word) > 0
+                        // && preg_match("/^(?=.*[A-Za-z])[\dA-Za-z\"'!@.,;:=_-]*$/", $word) //нужно что бы строка начиналась ли цифры или с англиской буквы а потом может содержать разные знаки разделители, стркоа обязательно должна иметь буквы
+                        && preg_match("/^(?=.*[A-Za-z])[\dA-Za-z[:punct:]]*$/", $word) //нужно что бы строка начиналась ли цифры или с англиской буквы а потом может содержать разные спец символы(!"#$%&'()*+,-./:;<=>?@[\]^_\`{|}~), стркоа обязательно должна иметь буквы
+                    ) {
+                        // $extractedWords[$index][$urls[$index]][] = $word;
+                        $extractedWords[$urls[$index]][] = $word;
+                    }
+                }
+            },
+            'rejected' => function (RequestException $ex, $index) use ($urls, $errorScrapingLogsFilePath, &$pageFailUrlsByScraping) {
+                $url = $urls[$index];
+                $message = "не удалось спарсить страницу: {$url} под номером {$index} , ошибка:{$ex?->getMessage()}. код:{$ex?->getCode()} в файле:{$ex?->getFile()} на строке: {$ex?->getLine()} \n";
+                $pageFailUrlsByScraping[$url] = $message;
+
+                file_put_contents($errorScrapingLogsFilePath, $message, FILE_APPEND);
             }
-        }
+        ]);
 
-        // sort($extractedWords, SORT_STRING);
+        $pool->promise()->wait();
+
+        // ksort($extractedWords);
 
         return $extractedWords;
     }
